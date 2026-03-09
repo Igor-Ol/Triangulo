c47f432ee6311be753d6448635f65f6dc5530048
Igor-Ol <igor.oliv.perei@gmail.com>
2026-03-08 22:43:05 -0300

    Nadador

---
+++ idadenadador.py
@@ -0,0 +1,14 @@
+idade = int(input("Digite sua idade: "))
+
+if idade <= 5 :
+    print("Não pode nadar")
+elif idade >= 5 and idade <= 7 :
+    print("Categoria infantil")
+elif idade >= 8 and idade <= 10 :
+    print("Categoria infantil")
+elif idade >= 11 and idade <= 15 :
+    print("Categoria infantil")
+elif idade >= 16 and idade <= 30 :
+    print("Categoria infantil")
+else :
+    print("Categoria Sênior")
